60c1e2b0d69e94b5e7f938d8740fed805ce45ea3
yeab <yeabmin@gmail.com>
2022-05-01 02:04:59 -0500

    Anima

--- ascii.js
+++ ascii.js
@@ -1,94 +1,98 @@
 window.onload = function(){
     "use strict";
-}
-let intervalAnim;
-var textArea;
-
-function start()
-{
+    var intervalAnim;
+    var textArea;
+    var stop;
+    var start;
     
-    textArea = document.getElementById("text-area").value;
-    let textAreaField = document.getElementById("text-area");
-    let toBeAnimated = document.getElementById("animation").value;
-    if(textAreaField.value == "" && toBeAnimated =="Blank")
+    const animate = function(a,b){
+        var i = 0;
+        var speed = document.getElementById("turbo").checked;
+        var animSpeed = 250;
+        if(speed)
+        {
+              animSpeed = 50;
+        }
+        intervalAnim =  setInterval(function(){
+            var val = a[i];
+            i = (i + 1) % a.length;
+            b.value = val;
+        }, animSpeed);
+    };
+    
+    start = function()
     {
-        textAreaField.value  = "Please choose an animation or write your own animation here.";
-        document.getElementById("stop").disabled = true;
-        document.getElementById("start").disabled = false;
-    }
-    else{
-        document.getElementById("stop").disabled = false;
-        document.getElementById("start").disabled = true;
-        let animElements = textAreaField.value;
-        let anim = animElements.split("=====\n");
-       
-            animate(anim, textAreaField);      
-    }
-}
-
-function stop()
-{
-    document.getElementById("start").disabled = false;
-    document.getElementById("stop").disabled = true;
-    clearInterval(intervalAnim);
-    document.getElementById("text-area").value = textArea;
-}
-
-function animationSelector()
-{
-    let val = document.getElementById("animation");
-    let selected = val.value;
-    document.getElementById("text-area").value = ANIMATIONS[selected];
+        var message = "Please choose an animation or write your " +
+                    "own animation here separated by 5" +
+                    "equal signs and a newline('=====/n') for each frame.";
+        textArea = document.getElementById("text-area").value;
+        var textAreaField = document.getElementById("text-area");
+        var toBeAnimated = document.getElementById("animation").value;
+        if(textAreaField.value === "" && toBeAnimated ==="Blank")
+        {
+            textAreaField.value  = message;
+            document.getElementById("stop").disabled = true;
+            document.getElementById("start").disabled = false;
+        }
+        else{
+            document.getElementById("stop").disabled = false;
+            document.getElementById("start").disabled = true;
+            var animElements = textAreaField.value;
+            var anim = animElements.split("=====\n");
+            animate(anim, textAreaField);  
+        }
+    };
    
-}
-
-function animate(a, b)
-{
-      let speed = document.getElementById("turbo").checked;
-      let animSpeed = 250;
-      if(speed)
-      {
-            animSpeed = 50;
-      }
-      var i = 0;
-      intervalAnim =  setInterval(function(){
-          let val = a[i];
-          i = (i + 1) % a.length;
-          
-          b.value = val;
-      }, animSpeed);
-
-}
-function fontSelector()
-{
-    let size = document.getElementById("fontsize").value;
     
 
-    if(size == "Tiny")
-    {
-        document.getElementById("text-area").style.fontSize = "7pt";
-    }
-    if(size == "Small")
-    {
-        document.getElementById("text-area").style.fontSize = "10pt";
-    }
-    if(size == "Medium")
-    {
-        document.getElementById("text-area").style.fontSize = "12pt";
-    }
-    if(size == "Large")
-    {
-        document.getElementById("text-area").style.fontSize = "16pt";
-    }
-    if(size == "Extra Large")
-    {
-        document.getElementById("text-area").style.fontSize = "24pt";
-    }
-    if(size == "XXL")
-    {
-        document.getElementById("text-area").style.fontSize = "32pt";
-    }
-}
-
-
-
+    stop =  function(){
+        document.getElementById("start").disabled = false;
+        document.getElementById("stop").disabled = true;
+        if(intervalAnim != null)
+        {
+            clearInterval(intervalAnim);
+        }
+       
+        document.getElementById("text-area").value = textArea;
+    };
+    
+    document.getElementById("turbo").onchange = function(){
+        stop();
+        start();
+    };
+    document.getElementById("animation").onchange = function(){
+        stop();
+        var val = document.getElementById("animation");
+        var selected = val.value;
+        document.getElementById("text-area").value = ANIMATIONS[selected];
+    };
+    document.getElementById("fontsize").onchange = function(){
+        var size = document.getElementById("fontsize").value;
+        if(size === "Tiny")
+        {
+            document.getElementById("text-area").style.fontSize = "7pt";
+        }
+        if(size === "Small")
+        {
+            document.getElementById("text-area").style.fontSize = "10pt";
+        }
+        if(size === "Medium")
+        {
+            document.getElementById("text-area").style.fontSize = "12pt";
+        }
+        if(size === "Large")
+        {
+            document.getElementById("text-area").style.fontSize = "16pt";
+        }
+        if(size === "Extra Large")
+        {
+            document.getElementById("text-area").style.fontSize = "24pt";
+        }
+        if(size === "XXL")
+        {
+            document.getElementById("text-area").style.fontSize = "32pt";
+        }
+    };
+    document.getElementById("start").onclick = start;
+    document.getElementById("stop").onclick = stop;
+};
